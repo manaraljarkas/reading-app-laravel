a6ffee7a3cb608f7497cdeb8113a6c3976e96a51
manar_aljarkas <manaraljarkas@gmail.com>
2025-06-29 15:18:25 +0300

    make some edits to avoid conflicts

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -8,7 +8,9 @@ use App\Models\Challenge;
 use App\Models\Comment;
 use App\Models\Reader;
 use App\Models\User;
+use App\Models\BookChallenge;
 use App\Helpers\CountryHelper;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
@@ -27,11 +29,17 @@ class BookController extends Controller
     public function getBooksComments($BookId)
     {
         $reader = Auth::user();
-        $comments = Comment::where('book_id', '=', $BookId)->get();
+        $comments = Comment::where('book_id', '=', $BookId)->with('reader')->get();
 
-        return response()->json(
-            $comments
-        );
+        $comments = $comments->map(function ($comment) {
+            return [
+                'reader_id' => $comment->reader_id,
+                'reader_name' => $comment->reader?->first_name,
+                'reader_image' => asset('storage/' . $comment->reader?->image),
+                'comment' => $comment->comment,
+            ];
+        });
+        return response()->json($comments);
     }
 
     public function getNumbers()
@@ -46,11 +54,127 @@ class BookController extends Controller
 
         return response()->json([
             'number_of_readers' => $readers,
+            'number_of_admins' => $admins,
             'number_of_books' => $books,
             'number_of_challenges' => $challenges,
             'number_of_categories' => $categories,
-            'number_of_admins' => $admins
         ]);
+    }
+
+    public function getBooks()
+    {
+        $user = Auth::user();
+        $books = Book::with('category', 'author')
+            ->withcount('readers')
+            ->paginate(10)
+            ->through(function ($book) {
+                return [
+                    'id' => $book->id,
+                    'title' => $book->title,
+                    'author_name' => $book->author?->name,
+                    'category' => $book->category?->name,
+                    'publish_date' => $book->publish_date,
+                    'star_rate' => $book->star_rate,
+                    'number_of_readers' => $book->readers_count,
+                ];
+            });
+
+        return response()->json($books);
+    }
+
+    public function getBookInfo($bookId)
+    {
+        $user = Auth::user();
+        $book = Book::where('id', '=', $bookId)->with('sizecategory')->with('bookChallenges')->first();
+        $number_of_participants = DB::table('reader_books')->where('reader_books.book_id', '=', $bookId)->count();
+
+        $comments = Comment::where('book_id', '=', $bookId)
+            ->with('reader')
+            ->get()
+            ->map(function ($comment) {
+                return [
+                    'image' => asset('storage/' . $comment->reader?->image),
+                    'name' => $comment->reader?->first_name,
+                    'comment' => $comment->comment,
+                ];
+            });
+
+        return response()->json([
+            'size_Category' => $book->sizecategory->name,
+            'description' => $book->description,
+            'summary' => $book->summary,
+            'book_challenge_duration' => $book->bookChallenges->duration,
+            'book_challenge_points' => $book->bookChallenges->points,
+            'book_challenge_participants' => $number_of_participants,
+            'comments' => $comments,
+        ]);
+    }
+
+    public function deleteBook($bookId)
+    {
+        $user = Auth::user();
+        $book = Book::find($bookId);
+
+        if (!$book) {
+            return response()->json(['message' => 'Book not found'], 404);
+        }
+        $book->delete();
+        return response()->json(['message' => 'book deleted successufly']);
+    }
+
+    public function addBook(Request $request)
+    {
+        $user = Auth::user();
+        DB::transaction(function () use ($request) {
+            $validated = $request->validate([
+                'title.en' => 'required|string',
+                'title.ar' => 'required|string',
+                'author_id' => 'required|integer|exists:authors,id',
+                'description.en' => 'required|string',
+                'description.ar' => 'required|string',
+                'category_id' => 'required|integer|exists:categories,id',
+                'publish_date' => 'required|date',
+                'number_of_pages' => 'integer|required',
+                'size_category_id' => 'integer|required|exists:size_categories,id',
+                'summary' => 'sometimes|string',
+                'book_file' => 'required|file',
+                'cover_image' => 'required|image',
+                'challenge_duration' => 'required|integer',
+                'challenge_points' => 'required|integer',
+                'description_BookChallenge.en' => 'required|string',
+                'description_BookChallenge.ar' => 'required|string',
+            ]);
+            $filepath = $request->file('book_file')->store('storage/books/pdfs', 'public');
+            $coverpath = $request->file('cover_image')->store('storage/books/covers', 'public');
+            $book = Book::create([
+                'title' => [
+                    'en' => $request->input('title')['en'],
+                    'ar' => $request->input('title')['ar'],
+                ],
+                'description' => [
+                    'en' => $request->input('description')['en'],
+                    'ar' => $request->input('description')['ar'],
+                ],
+                'author_id' => $request->author_id,
+                'publish_date' => $request->publish_date,
+                'number_of_pages' => $request->number_of_pages,
+                'summary' => $request->summary,
+                'book_pdf' => $filepath,
+                'cover_image' => $coverpath,
+                'size_category_id' => $request->size_category_id,
+                'category_id' => $request->category_id,
+            ]);
+
+            BookChallenge::create([
+                'duration' => $request->challenge_duration,
+                'points' => $request->challenge_points,
+                'description' => [
+                    'en' => $request->input('description_BookChallenge')['en'],
+                    'ar' => $request->input('description_BookChallenge')['ar'],
+                ],
+                'book_id' => $book->id,
+            ]);
+        });
     }
 
     public function getMostRatedBooks()
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\BagdeController;
 use App\Http\Controllers\CategotyController;
 use App\Http\Controllers\ChallengesController;
 use App\Http\Controllers\ComplaintController;
@@ -22,45 +23,69 @@ Route::post('/dashboard/login', [AuthController::class, 'webLogin']);
 
 
 //---------------------Authenticated  routes---------------------------
-Route::middleware('auth:sanctum')->group(function()
-{
-//--------------------------Auth--------------------------
-Route::post('logout', [AuthController::class, 'logout']);
-Route::post('auth/setup-profile', [AuthController::class, 'setupProfile']);
-Route::post('auth/edit-profile', [AuthController::class, 'editProfile']);
+Route::middleware('auth:sanctum')->group(function () {
+    //--------------------------Auth--------------------------
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('auth/setup-profile', [AuthController::class, 'setupProfile']);
+    Route::post('auth/edit-profile', [AuthController::class, 'editProfile']);
 
-//--------------------------Author------------------------
-Route::get('/getAuthors',[AuthorController::class,'index']);
+    //--------------------------Author------------------------
+    Route::get('/getAuthors', [AuthorController::class, 'index']);
+    Route::get('/getAuthors_D', [AuthorController::class, 'getAuthors_D']);
+    Route::post('/AddAuthor_D', [AuthorController::class, 'AddAuthor']);
+    Route::post('/editAuthor/{id}', [AuthorController::class, 'editAuthor']);
+    Route::delete('/deleteAuthor_D/{authorId}', [AuthorController::class, 'deleteAuthor']);
 
-//---------------------------Book--------------------------
-Route::get('book/getBookFile/{BookId}',[BookController::class,'getBookFile']);
-Route::get('book/getBooksComments/{BookId}',[BookController::class,'getBooksComments']);
-Route::get('book/getNumbers',[BookController::class,'getNumbers']);
-Route::get('book/getMostRatedBooks', [BookController::class, 'getMostRatedBooks']);
-Route::get('book/getAuthorBooks/{authorId}', [BookController::class, 'getAuthorBooks']);
-Route::get('book/getCategoryBooks/{categoryId}', [BookController::class, 'getCategoryBooks']);
+    //---------------------------Book--------------------------
+    Route::get('book/getBookFile/{BookId}', [BookController::class, 'getBookFile']);
+    Route::get('book/getBooksComments/{BookId}', [BookController::class, 'getBooksComments']);
+    Route::get('book/getNumbers', [BookController::class, 'getNumbers']);
+    Route::get('book/getMostRatedBooks', [BookController::class, 'getMostRatedBooks']);
+    Route::get('book/getAuthorBooks/{authorId}', [BookController::class, 'getAuthorBooks']);
+    Route::get('book/getCategoryBooks/{categoryId}', [BookController::class, 'getCategoryBooks']);
+    Route::get('book/getBooks', [BookController::class, 'getBooks']);
+    Route::post('book/addBook', [BookController::class, 'addBook']);
+    Route::delete('book/deleteBook/{bookId}', [BookController::class, 'deleteBook']);
+    Route::get('book/getbookinfo/{bookId}', [BookController::class, 'getbookinfo']);
 
-//----------------------------Category----------------------------
-Route::get('/getCategories',[CategotyController::class,'index']);
+    //----------------------------Category----------------------------
+    Route::get('/getCategories', [CategotyController::class, 'index']);
+    Route::post('/addcategory', [CategotyController::class, 'addcategory']);
+    Route::post('/editCategory/{id}', [CategotyController::class, 'editCategory']);
 
-//----------------------------Challenge--------------------------------
-Route::get('/getchallenges',[ChallengesController::class,'index']);
+    //----------------------------Challenge--------------------------------
+    Route::get('/getchallenges', [ChallengesController::class, 'index']);
+    Route::get('/getchallenges_D', [ChallengesController::class, 'getchallenges']);
+    Route::post('/editchallenge/{id}', [ChallengesController::class, 'editchallenge']);
+    Route::post('/addChallenge', [ChallengesController::class, 'addChallenge']);
+    Route::get('/getchallengeinfo/{id}', [ChallengesController::class, 'getchallengeinfo']);
+    Route::delete('/deleteChallenge_D/{challengeId}', [ChallengesController::class, 'deleteChallenge']);
 
-//----------------------------Suggestion----------------------------
-Route::get('/getsuggestions',[SuggestionController::class,'index']);
-Route::post('/UpdateSuggestion/{suggestionId}',[SuggestionController::class,'UpdateSuggestion']);
-Route::get('/getSuggestionInfo/{suggestionId}',[SuggestionController::class,'getSuggestionInfo']);
+    //----------------------------Suggestion----------------------------
+    Route::get('/getsuggestions', [SuggestionController::class, 'index']);
+    Route::post('/UpdateSuggestion/{suggestionId}', [SuggestionController::class, 'UpdateSuggestion']);
+    Route::get('/getSuggestionInfo/{suggestionId}', [SuggestionController::class, 'getSuggestionInfo']);
+    Route::delete('/deleteSuggestion_D/{suggestionId}', [SuggestionController::class, 'deleteSuggestion']);
 
-//----------------------------Complaint-------------------------------
-Route::get('/getComplaints',[ComplaintController::class,'getComplaints']);
+    //----------------------------Complaint-------------------------------
+    Route::get('/getComplaints', [ComplaintController::class, 'getComplaints']);
 
-//----------------------------Admin--------------------------------------
-Route::get('/getAdminInfo/{adminId}',[UserController::class,'getAdminInfo']);
-Route::post('/AddAdmin',[UserController::class,'AddAdmin']);
-Route::delete('/deleteAdmin/{Adminid}',[UserController::class,'deleteAdmin']);
+    //----------------------------Admin--------------------------------------
+    Route::get('/getAdminInfo/{adminId}', [UserController::class, 'getAdminInfo']);
+    Route::post('/AddAdmin', [UserController::class, 'AddAdmin']);
+    Route::delete('/deleteAdmin/{Adminid}', [UserController::class, 'deleteAdmin']);
+    Route::get('/getAdmins', [UserController::class, 'getAdmins']);
+    Route::get('/getAdminInfo/{adminId}', [UserController::class, 'getAdminInfo']);
 
-//-----------------------------Reader-------------------------------------
-Route::get('/getReaderInfo/{readerId}',[ReaderController::class,'getReaderInfo']);
-Route::delete('/DeleteReader/{readerId}',[ReaderController::class,'DeleteReader']);
+    //-----------------------------Reader-------------------------------------
+    Route::get('/getReaderInfo/{readerId}', [ReaderController::class, 'getReaderInfo']);
+    Route::delete('/DeleteReader/{readerId}', [ReaderController::class, 'DeleteReader']);
+    Route::get('/getReaders', [ReaderController::class, 'getReaders']);
+    Route::get('/getReaderInfo/{readerId}', [ReaderController::class, 'getReaderInfo']);
 
+    //-----------------------------Badge-------------------------------------
+    Route::get('/getBadges_D', [BagdeController::class, 'getBadges']);
+    Route::delete('/deletebadge_D/{badgeId}', [BagdeController::class, 'deletebadge']);
+    Route::post('/addBadge_D', [BagdeController::class, 'addBadge']);
+    Route::post('/editBadge/{id}', [BagdeController::class, 'editBadge']);
 });
