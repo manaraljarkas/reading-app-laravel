2119b973059316054b9a5a482f92c181a4737c87
manar_aljarkas <manaraljarkas@gmail.com>
2025-07-02 13:51:17 +0300

    fix code column duplicate in countries table

--- database/migrations/2025_05_26_090644_create_countries_table.php
+++ database/migrations/2025_05_26_090644_create_countries_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('countries', function (Blueprint $table) {
             $table->id();
             $table->json('name');
-            $table->string('code');
+            $table->string('code', 2)->nullable();
             $table->float('longitude')->nullable();
             $table->float('latitude')->nullable();
             $table->timestamps();
